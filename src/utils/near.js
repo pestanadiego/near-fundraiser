56d4153eab2af002752acb9b392e98ca60d89c0f
diegopestana <pestanadiegoalberto@gmail.com>
2022-07-29 19:12:19 -0400

    end result

---
+++ src/utils/near.js
@@ -0,0 +1,49 @@
+import environment from "./config";
+import {
+  connect,
+  Contract,
+  keyStores,
+  WalletConnection,
+  utils,
+} from "near-api-js";
+
+const nearEnv = environment("testnet");
+
+export async function initializeContract() {
+  const near = await connect(
+    Object.assign(
+      { deps: { keyStore: new keyStores.BrowserLocalStorageKeyStore() } },
+      nearEnv
+    )
+  );
+  window.walletConnection = new WalletConnection(near);
+  window.accountId = window.walletConnection.getAccountId();
+  window.contract = new Contract(
+    window.walletConnection.account(),
+    nearEnv.contractName,
+    {
+      viewMethods: ["getProduct", "getProducts"],
+      changeMethods: ["buyProduct", "setProduct"],
+    }
+  );
+}
+
+export async function accountBalance() {
+  return utils.format.formatNearAmount(
+    (await window.walletConnection.account().getAccountBalance()).total,
+    2
+  );
+}
+
+export async function getAccountId() {
+  return window.walletConnection.getAccountId();
+}
+
+export function login() {
+  window.walletConnection.requestSignIn(nearEnv.contractName);
+}
+
+export function logout() {
+  window.walletConnection.signOut(nearEnv.contractName);
+  window.location.reload();
+}
