99dc6f178e5ef4e4d1d48528421b1afbc492a7c9
FarzeenKist <farzeenkistnareddy@gmail.com>
2022-08-13 14:51:34 +0400

    fixed issues and improved smart contract

--- contract/assembly/index.ts
+++ contract/assembly/index.ts
@@ -1,6 +1,11 @@
 import { Donation, Fundraise, listedFundraises } from './model';
 import { ContractPromiseBatch, context } from 'near-sdk-core';
 
+
+/**
+ * @dev allows users to create a Fundraise
+ * @param fundraise Object of values to create a new instance of Fundraise
+ */
 export function setFundraise(fundraise: Fundraise): void {
     let storedFundraise = listedFundraises.get(fundraise.id);
     if(storedFundraise !== null) {
@@ -17,44 +22,63 @@ export function getFundraises(): Fundraise[] {
     return listedFundraises.values();
 }
 
-export function upvote(fundraiserId: string, wallet: string): void {
-    const fundraise = getFundraise(fundraiserId);
+/**
+ * @dev allows user to upvote a fundraise
+ * @param fundraiseId id of fundraise
+ */
+export function upvote(fundraiseId: string): void {
+    const fundraise = getFundraise(fundraiseId);
     if(fundraise == null) {
         throw new Error("Fundraise not found");
     // In case there are upvotes and the wallet already upvoted, an error is throwed
-    } else if(fundraise.upvotes && fundraise.upvotes.indexOf(wallet) >= 0) {
-        throw new Error("Already upvoted");
     }
-    // When a Fundraise instance is initializated, it does not have the Upvotes Array created.
-    if(fundraise.upvotes === null) {
-        fundraise.upvotes = [];
-    }
-    fundraise.upvotes.push(wallet);
+    assert(fundraise.ended == false, "Fundraise is over");
+    const wallet: string = context.sender.toString();
+    assert(fundraise.upvotes.indexOf(wallet) == -1, "Already Upvoted");
+    fundraise.upvote(wallet);
     listedFundraises.set(fundraise.id, fundraise); 
 }
 
-export function makeDonation(fundraiserId: string, donation: Donation): void {
-    const fundraise = getFundraise(fundraiserId);
+/**
+ * @dev allows users to make a donation to a campaign
+ * @param fundraiseId id of fundraise
+ * @param donation Object containing values necessary to create an instance of Donation
+ * @notice If user hasn't upvoted campaign yet, by donating, they automatically upvote fundraise
+ */
+export function makeDonation(fundraiseId: string, donation: Donation): void {
+    const fundraise = getFundraise(fundraiseId);
     if(fundraise == null) {
         throw new Error("Fundraise not found");
     }
-    ContractPromiseBatch.create(fundraise.owner).transfer(context.attachedDeposit);
-    // When a Fundraise instance is initializated, it does not have the Donations Array created.
-    if(fundraise.donations === null) {
-        fundraise.donations = [];
-    }
-    fundraise.donations.push(donation);
+    assert(fundraise.ended == false, "Fundraise is over");
+    assert(donation.wallet.toString() != "", "Invalid wallet id");
+    assert(donation.amount.toString() == context.attachedDeposit.toString(), "Amount donated has to match the attached Deposit");
+    // donations is pushed onto the donations array
+    fundraise.donate(donation);
     // By donating, it automatically counts an upvote
-    let alreadyUpvoted = false;
-    // In case the Upvotes Array has not been already created
-    if(fundraise.upvotes === null) {
-        fundraise.upvotes = [];
-    // If the wallet donating has already upvoted the fundraise, it does not add a new upvote.
-    } else if(fundraise.upvotes && fundraise.upvotes.indexOf(donation.wallet) >= 0) {
-        alreadyUpvoted = true;
+    // If wallet hasn't upvoted yet, upvote for wallet is added
+    if(fundraise.upvotes.indexOf(donation.wallet) == -1){
+        fundraise.upvote(donation.wallet);
     }
-    if(!alreadyUpvoted) {
-        fundraise.upvotes.push(donation.wallet);
-    }
+
     listedFundraises.set(fundraise.id, fundraise);
+}
+
+
+/**
+ * @dev allows the fundraise's owner to end the fundraise
+ * @param fundraiseId id of fundraise
+ * @notice fundraise can only be ended after amount collected is greater or equal to the amount Needed
+ */
+export function endFundraise(fundraiseId: string): void {
+    const fundraise = getFundraise(fundraiseId);
+    if(fundraise == null) {
+        throw new Error("Fundraise not found");
+    }
+
+    assert(fundraise.owner.toString() == context.sender.toString(), "Only the owner of the fundraise can end the fundraise");
+    assert(fundraise.ended == false, "Fundraise is over");
+    assert(fundraise.collected >= fundraise.amountNeeded, "Not enough collected yet from the fundraise");
+    ContractPromiseBatch.create(fundraise.owner).transfer(fundraise.collected);
+    fundraise.end();
 }
--- src/components/forum/AddFundraise.js
+++ src/components/forum/AddFundraise.js
@@ -4,127 +4,127 @@ import toast from "react-hot-toast";
 import { Button, Modal, Form, FloatingLabel } from "react-bootstrap";
 
 const AddFundraise = ({ save, account }) => {
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
-  const [image, setImage] = useState("");
-  const [amountNeeded, setAmountNeeded] = useState("");
-  const isFormFilled = () => name && image && description && amountNeeded;
+	const [name, setName] = useState("");
+	const [description, setDescription] = useState("");
+	const [image, setImage] = useState("");
+	const [amountNeeded, setAmountNeeded] = useState("");
+	const isFormFilled = () => name && image && description && amountNeeded;
 
-  const [show, setShow] = useState(false);
+	const [show, setShow] = useState(false);
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => {
-    if (account) {
-      setShow(true);
-    } else {
-      toast.error("You need to connect your wallet first.");
-    }
-  };
+	const handleClose = () => setShow(false);
+	const handleShow = () => {
+		if (account) {
+			setShow(true);
+		} else {
+			toast.error("You need to connect your wallet first.");
+		}
+	};
 
-  return (
-    <>
-      <Button
-        onClick={handleShow}
-        variant='dark'
-        style={{ display: "flex", alignItems: "baseline" }}
-      >
-        Add one
-        <i style={{ marginLeft: "5px" }} class='bi bi-plus'></i>
-      </Button>
-      <Modal show={show} onHide={handleClose} centered>
-        <Modal.Header closeButton>
-          <Modal.Title>New Fundraise</Modal.Title>
-        </Modal.Header>
-        <Form>
-          <Modal.Body>
-            <FloatingLabel
-              controlId='inputName'
-              label='Fundraiser name'
-              className='mb-3'
-            >
-              <Form.Control
-                type='text'
-                onChange={(e) => {
-                  setName(e.target.value);
-                }}
-                placeholder='Enter name of fundraise'
-              />
-            </FloatingLabel>
-            <FloatingLabel
-              controlId='inputUrl'
-              label='Image URL'
-              className='mb-3'
-            >
-              <Form.Control
-                type='text'
-                placeholder='Image URL'
-                onChange={(e) => {
-                  setImage(e.target.value);
-                }}
-              />
-            </FloatingLabel>
-            <FloatingLabel
-              controlId='inputDescription'
-              label='Description'
-              className='mb-3'
-            >
-              <Form.Control
-                as='textarea'
-                placeholder='description'
-                style={{ height: "80px" }}
-                onChange={(e) => {
-                  setDescription(e.target.value);
-                }}
-              />
-            </FloatingLabel>
-            <FloatingLabel
-              controlId='inputAmountNeeded'
-              label='Amount Needed'
-              className='mb-3'
-            >
-              <Form.Control
-                type='text'
-                placeholder='Amount Needed'
-                onChange={(e) => {
-                  setAmountNeeded(e.target.value);
-                }}
-              />
-            </FloatingLabel>
-          </Modal.Body>
-        </Form>
-        <Modal.Footer>
-          <Button variant='outline-secondary' onClick={handleClose}>
-            Close
-          </Button>
-          <Button
-            variant='dark'
-            disabled={!isFormFilled()}
-            onClick={() => {
-              save({
-                name,
-                description,
-                image,
-                amountNeeded,
-              });
-              console.log({
-                name,
-                description,
-                image,
-                amountNeeded,
-              });
-              handleClose();
-            }}
-          >
-            Save
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
-  );
+	return (
+		<>
+			<Button
+				onClick={handleShow}
+				variant="dark"
+				style={{ display: "flex", alignItems: "baseline" }}
+			>
+				Add one
+				<i style={{ marginLeft: "5px" }} className="bi bi-plus"></i>
+			</Button>
+			<Modal show={show} onHide={handleClose} centered>
+				<Modal.Header closeButton>
+					<Modal.Title>New Fundraise</Modal.Title>
+				</Modal.Header>
+				<Form>
+					<Modal.Body>
+						<FloatingLabel
+							controlId="inputName"
+							label="Fundraiser name"
+							className="mb-3"
+						>
+							<Form.Control
+								type="text"
+								onChange={(e) => {
+									setName(e.target.value);
+								}}
+								placeholder="Enter name of fundraise"
+							/>
+						</FloatingLabel>
+						<FloatingLabel
+							controlId="inputUrl"
+							label="Image URL"
+							className="mb-3"
+						>
+							<Form.Control
+								type="text"
+								placeholder="Image URL"
+								onChange={(e) => {
+									setImage(e.target.value);
+								}}
+							/>
+						</FloatingLabel>
+						<FloatingLabel
+							controlId="inputDescription"
+							label="Description"
+							className="mb-3"
+						>
+							<Form.Control
+								as="textarea"
+								placeholder="description"
+								style={{ height: "80px" }}
+								onChange={(e) => {
+									setDescription(e.target.value);
+								}}
+							/>
+						</FloatingLabel>
+						<FloatingLabel
+							controlId="inputAmountNeeded"
+							label="Amount Needed"
+							className="mb-3"
+						>
+							<Form.Control
+								type="text"
+								placeholder="Amount Needed"
+								onChange={(e) => {
+									setAmountNeeded(e.target.value);
+								}}
+							/>
+						</FloatingLabel>
+					</Modal.Body>
+				</Form>
+				<Modal.Footer>
+					<Button variant="outline-secondary" onClick={handleClose}>
+						Close
+					</Button>
+					<Button
+						variant="dark"
+						disabled={!isFormFilled()}
+						onClick={() => {
+							save({
+								name,
+								description,
+								image,
+								amountNeeded,
+							});
+							console.log({
+								name,
+								description,
+								image,
+								amountNeeded,
+							});
+							handleClose();
+						}}
+					>
+						Save
+					</Button>
+				</Modal.Footer>
+			</Modal>
+		</>
+	);
 };
 
 AddFundraise.propTypes = {
-  save: PropTypes.func.isRequired,
+	save: PropTypes.func.isRequired,
 };
 
 export default AddFundraise;
